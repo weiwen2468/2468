d807198c8766cbeac8935118baa1c166254694b2
weiwen2468 <weiwen52911@gmail.com>
2024-02-07 18:22:58 -0500

    Cleaned

--- src/Problem_J1.java
+++ src/Problem_J1.java
@@ -4,25 +4,19 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Problem_J1 {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
 
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        Scanner myObj = new Scanner(System.in);
         System.out.println("Enter P:");
 
-        int P = Integer.parseInt(myObj.nextLine());  // Read user input
-        System.out.println("Points is: " + P);  // Output user input
+        int P = Integer.parseInt(myObj.nextLine());
+        System.out.println("Points is: " + P);
 
-        Scanner myObj2 = new Scanner(System.in);  // Create a Scanner object
+        Scanner myObj2 = new Scanner(System.in);
         System.out.println("Enter C:");
 
-        int C = Integer.parseInt(myObj2.nextLine());  // Read user input
-        System.out.println("Errors is: " + C);  // Output user input
+        int C = Integer.parseInt(myObj2.nextLine());
+        System.out.println("Errors is: " + C);
 
-
-//        int P = 0;
-//        int C = 10;
 
         int points_gain = P * 50;
         int points_lost = C * 10;
@@ -35,10 +29,6 @@ public class Problem_J1 {
 
         System.out.println("Output = " + final_points);
 
-//        for (int i = 1; i <= 5; i++) {
-//            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-//            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-//            System.out.println("i = " + i);
-//        }
+
     }
 }
--- src/Problem_J2.java
+++ src/Problem_J2.java
@@ -7,21 +7,18 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Problem_J2 {
     public static void main(String[] args) {
-//        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-//        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
 
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        Scanner myObj = new Scanner(System.in);
         System.out.println("Enter Number of Peppers:");
 
-        int N = Integer.parseInt(myObj.nextLine());  // Read user input
-        System.out.println("Number of Peppers: " + N);  // Output user input
+        int N = Integer.parseInt(myObj.nextLine());
+        System.out.println("Number of Peppers: " + N);
 
-        Scanner myObj2 = new Scanner(System.in);  // Create a Scanner object
+        Scanner myObj2 = new Scanner(System.in);
         System.out.println("Enter Name of Peppers:");
 
-        String s = myObj2.nextLine();  // Read user input
-        System.out.println("Name of Peppers " + s);  // Output user input
+        String s = myObj2.nextLine();
+        System.out.println("Name of Peppers: " + s);
 
 
         List<String> myList = new ArrayList<>(Arrays.asList(s.split(" ")));
@@ -56,34 +53,5 @@ public class Problem_J2 {
         }
         System.out.println("Ron's chill's heat: " + output);
 
-
-
-//
-//        Scanner myObj2 = new Scanner(System.in);  // Create a Scanner object
-//        System.out.println("Enter C:");
-//
-//        int C = Integer.parseInt(myObj2.nextLine());  // Read user input
-//        System.out.println("Errors is: " + C);  // Output user input
-//
-//
-////        int P = 0;
-////        int C = 10;
-//
-//        int points_gain = P * 50;
-//        int points_lost = C * 10;
-//
-//        if (P > C) {
-//            points_gain = points_gain + 500;
-//        }
-//
-//        int final_points = points_gain - points_lost;
-//
-//        System.out.println("Output = " + final_points);
-
-//        for (int i = 1; i <= 5; i++) {
-//            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-//            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-//            System.out.println("i = " + i);
-//        }
     }
 }
